4c35424421fc2288939e9f2e38179c4806655265
DomamaN202 <vip.domaman@mail.ru>
2022-08-08 14:34:52 +0700

    =)

--- src/main/groovy/ru/uris/gadapter/GroovySocket.groovy
+++ src/main/groovy/ru/uris/gadapter/GroovySocket.groovy
@@ -4,10 +4,6 @@ package ru.uris.gadapter
 import ru.uris.RemoteMethod
 
 trait GroovySocket {
-    Object invokeRemoteMethod(RemoteMethod method, Object... args) throws IOException {
-        return this.getObjectPool().get(method.obj).invokeMethod(method.name, args)
-    }
-
     synchronized <T extends Enum<T>> T readEnum(Class<T> clazz) throws IOException {
         this.checkValue(6);
         var name = this.readStringI();
